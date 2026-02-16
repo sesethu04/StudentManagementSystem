3f1f6159c67e9dac43e3c52b096df32ca6007288
Sesethu <231118384@mycput.ac.za>
2026-02-16 23:09:18 +0200

    added successful

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -1,17 +1,34 @@
 package za.ac.cput;
 
+import za.ac.cput.domain.GraduateStudent;
+import za.ac.cput.domain.Student;
+import za.ac.cput.domain.UndergraduateStudent;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+        Student undergrad = new UndergraduateStudent.Builder(
+                "1234",
+                "Sesethu Nciti",
+                "23118384@mycput.ac.za",
+                "Application development"
+        )
+                .creditHours(15)
+                .scholarshipAmount(1000)
+                .build();
+
+        Student graduate = new GraduateStudent.Builder(
+                "2345",
+                "Yonelisa Ngudle",
+                "15664567@mycput.ac.za",
+                "Software Engineering"
+        )
+                .researchAssistant(true)
+                .stipend(2000)
+                .build();
+
+        undergrad.displayStudentDetails();
+        graduate.displayStudentDetails();    }
 }
